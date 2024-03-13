d0628d321f1f0ae3cd5d2fe7987f69a580125115
keith japitana <keithjapitana1996@gmail.com>
2024-03-13 15:42:50 +0800

    Intro to objects.

--- js/script.js
+++ js/script.js
@@ -183,14 +183,28 @@ function cutFruitPieces(fruit){
 //   console.log('keith is included');
 // }
 
-const calcTip = function (bill){
-  return bill >=50 && bill <= 300? bill *0.15
-  : bill *0.20;
-}
 
-const bills = [123, 555, 44];
+// //coding challenge
+// const calcTip = function (bill){
+//   return bill >=50 && bill <= 300? bill *0.15
+//   : bill *0.20;
+// }
 
-const tip = [calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])]
-const total = [bills[0]+tip[0],bills[1]+tip[1],bills[2]+tip[2]];
+// const bills = [123, 555, 44];
 
-console.log(bills,tip,total);
+// const tip = [calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])]
+// const total = [bills[0]+tip[0],bills[1]+tip[1],bills[2]+tip[2]];
+
+// console.log(bills,tip,total);
+
+// Objects 
+
+const jonas = {
+  firstName: 'Keith',
+  lastname: 'Japitana',
+  age: 2024-1996,
+  job: 'gambler',
+  friends:['one','two','three']
+};
+
+console.log(jonas);
