db5ee173af453f3edec0842b02c30888929c5d88
keith japitana <keithjapitana1996@gmail.com>
2024-03-15 01:28:28 +0800

    Handling clicks.

--- js/script.js
+++ js/script.js
@@ -434,12 +434,27 @@
 
 // //A identify the Bug
 // console.log(measureKelvin());
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number';
-console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 12;
-document.querySelector('.score').textContent = 13;
+//Selecting and manipulating elements.
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number';
+// console.log(document.querySelector('.message').textContent);
 
-console.log(document.querySelector('.guess').value );
-document.querySelector('.guess').value = 12;
+// document.querySelector('.number').textContent = 12;
+// document.querySelector('.score').textContent = 13;
+
+// console.log(document.querySelector('.guess').value );
+// document.querySelector('.guess').value = 12;
+
+//Handling click events
+document.querySelector('.check').addEventListener('click', function() {
+
+  const guess = Number (document.querySelector('.guess').value);
+  document.querySelector('.message').textContent = 'Correct number';
+  console.log(guess, typeof guess);
+
+  if(!guess){
+    document.querySelector('.message').textContent = 'Please input a number';
+  }
+
+});
